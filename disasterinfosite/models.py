7ac37f1e0589dda34e532649792b429b381e960b
nein09 <nein09@nein09.com>
2016-10-06 15:02:35 -0700

    Add caption to model and initial migration

--- disasterinfosite/models.py
+++ disasterinfosite/models.py
@@ -622,6 +622,7 @@ class EmbedSnugget(Snugget):
 class PastEventsPhoto(models.Model):
     group = models.ForeignKey(ShapefileGroup, on_delete=models.PROTECT, null=True)
     image = models.ImageField(upload_to="photos")
+    caption = models.TextField(default="", max_length=200)
 
     def __str__(self):
         return self.image.url
