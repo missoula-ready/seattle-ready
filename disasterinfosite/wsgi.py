ce0d49e44e7a8d87e01f04767b76d6492599e65b
missoulaready <hazard@hazardready.org>
2017-07-14 12:40:54 -0700

    Commit test server settings

--- disasterinfosite/wsgi.py
+++ disasterinfosite/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 os.environ["DJANGO_SETTINGS_MODULE"] = "disasterinfosite.settings"
+sys.path.append('/home/missoulaready/seattle-ready/venv/lib/python3.5/site-packages')
+sys.path.append('/home/missoulaready/seattle-ready')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
