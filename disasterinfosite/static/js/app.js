29d4e43cf3c5babc5189b3ebdc22f17237f7b9c2
nein09 <nein09@nein09.com>
2017-09-06 17:23:47 -0700

    Extract functions from document ready for speed

--- disasterinfosite/static/js/app.js
+++ disasterinfosite/static/js/app.js
@@ -12,6 +12,158 @@ require('../img/thinking.gif');
 require('slick-carousel');
 var $ = require('jquery');
 
+var $locationInput = $('#location-text');
+var $locationSubmit = $('#location-submit');
+var $autoLocationSubmit = $('.auto-location-submit');
+var $loading = $('.loading');
+
+// during api calls, disable the form
+function disableForm() {
+  $locationInput.prop("disabled", true);
+  $locationSubmit.addClass("disabled");
+  $autoLocationSubmit.addClass("disabled");
+  $loading.show();
+}
+
+// if a search fails or a restart, enable the form
+function enableForm() {
+  $locationInput.prop("disabled", false);
+  $locationSubmit.removeClass("disabled");
+  $autoLocationSubmit.removeClass("disabled");
+  $loading.hide();
+}
+
+function submitLocation(lat,lng, location_query_text) {
+  // reload the page with the lat,lng
+  document.location =  encodeURI(document.location.pathname + "?lat=" + lat + "&lng=" + lng + "&loc=" + location_query_text);
+}
+
+function submitLocationQuery() {
+  // grab the query value, ignoring it if it's empty
+  var location_query_text = $locationInput.val();
+  console.log(location_query_text);
+  if (location_query_text.length === 0) return;
+  disableForm();
+
+  // request geocoding from google CLIENT SIDE!
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode( { 'address': location_query_text}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var lat = results[0].geometry.location.lat();
+      var lon = results[0].geometry.location.lng();
+      submitLocation(lat,lon, location_query_text);
+    } else {
+      $(".geocode-error-message").html($('p').text("We had a problem finding that location."));
+    }
+  });
+};
+
+// This is on window so that it can get called after the google maps API script is loaded asynchronously.
+window.initAddressInput = function() {
+  // Set up autocomplete
+  var autocomplete = new google.maps.places.Autocomplete($locationInput[0]);
+
+  // submit location text
+  $locationSubmit.click(submitLocationQuery);
+
+  // hitting enter key in the textfield will also trigger submit
+  $locationInput.keydown(function(event) {
+    if (event.keyCode === 13) {
+      submitLocationQuery();
+      return false;
+    }
+  });
+};
+
+// convenience function to extract url parameters
+function getURLParameter(name) {
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results==null) {
+     return null;
+  } else {
+     return results[1] || 0;
+  }
+};
+
+// Set up slick photo slideshow
+function loadGallery() {
+  var currentSlideElement = $('.disaster-content.active .past-photos');
+  currentSlideElement.slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    prevArrow: '<button type="button" class="slick-prev"><</button>',
+    nextArrow: '<button type="button" class="slick-next">></button>'
+  });
+  return currentSlideElement;
+};
+
+// Helpers for the sign-up / log in form
+function setValueOnFocus(el, value) {
+  el.focus(function() {
+    if(el.val() === "") {
+      el.val(value);
+    }
+  });
+};
+
+function requiredFocus(el) {
+  el.focus(function() {
+    el.removeAttr('placeholder');
+  });
+};
+
+function requiredBlur(el, text) {
+  el.blur(function() {
+    if(el.val() === "") {
+      el.attr('placeholder', text);
+    }
+  });
+};
+
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = $.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+  }
+  return cookieValue;
+};
+
+function sendAjaxAuthRequest(url, data, error, success) {
+  var csrftoken = getCookie('csrftoken');
+  $.ajaxSetup({
+    crossDomain: false,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    }
+  });
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    error: error,
+    success: success
+  });
+};
+
+function hasInvalidInput($form) {
+  var inputs = $form.find('input:visible');
+  for(var i = 0; i < inputs.length; i++ ) {
+    if(!inputs[i].checkValidity()) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
 $( document ).ready(function() {
   $(document).foundation();
 
@@ -20,16 +172,6 @@ $( document ).ready(function() {
       return trackOutboundLink(e.currentTarget.href, e.currentTarget.target === "_blank");
     }
   });
-
-  // convenience function to extract url parameters
-  function getURLParameter(name) {
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    if (results==null) {
-       return null;
-    } else {
-       return results[1] || 0;
-    }
-  }
 
   // grab the position, if possible
   var query_lat = getURLParameter('lat');
@@ -61,15 +203,14 @@ $( document ).ready(function() {
   var layer = new L.TileLayer(osmUrl, {attribution: osmAttrib}).addTo(map);
   layer.setOpacity(0.6);
 
-  var boundaryStyle = {
-    "color": "rgb(165, 199, 39)",
-    "weight": 4,
-    "opacity": 1,
-    "fillColor": "#ffffff",
-    "fillOpacity": 0.7
-  };
-
-  L.geoJson(boundaryShape, { style: boundaryStyle }).addTo(map);
+  L.geoJson(boundaryShape,
+    { style: {
+        "color": "rgb(165, 199, 39)",
+        "weight": 4,
+        "opacity": 1,
+        "fillColor": "#ffffff",
+        "fillOpacity": 0.7
+    }}).addTo(map);
 
   document.getElementById('map').style.cursor='default';
   if (query_lat && query_lng) {
@@ -86,9 +227,8 @@ $( document ).ready(function() {
 
   // Make a click on the map submit the location
   map.on('click', function(e) {
-    location_query_text = "";
-    $("#location-text").val(location_query_text);  // clear query text
-    submitLocation(e.latlng.lat, e.latlng.lng);
+    $locationInput.val("");  // clear query text
+    submitLocation(e.latlng.lat, e.latlng.lng, "");
   });
 
   // grab and set any previously entered query text
@@ -96,51 +236,18 @@ $( document ).ready(function() {
   var location_query_text = (loc) ? decodeURIComponent(loc) : query_lat + "," + query_lng;
   if (!query_lat || !query_lng)
     location_query_text = "";
-  $("#location-text").val(location_query_text);
+  $locationInput.val(location_query_text);
 
-  // Set up autocomplete
-  var input = document.getElementById('location-text');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-
-
-  // hitting enter key in the textfield will trigger submit
-  $("#location-text").keydown(function(event) {
-    if (event.keyCode == 13) {
-      $('#location-submit').trigger('click');
-      return false;
-    }
-  });
-
-  // submit location text
-  $("#location-submit").click(function() {
-    // grab the query value, ignoring it if it's empty
-    location_query_text = $("#location-text").val();
-    if (location_query_text.length == 0) return;
-    disableForm();
-
-    // request geocoding from google CLIENT SIDE!
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { 'address': location_query_text}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        var lat = results[0].geometry.location.lat();
-        var lon = results[0].geometry.location.lng();
-        submitLocation(lat,lon);
-      } else {
-        $(".geocode-error-message").html($('p').text("We had a problem finding that location."));
-      }
-    });
-  });
 
   // auto location
-  $(".auto-location-submit").click(function() {
-    location_query_text = "";
+  $autoLocationSubmit.click(function() {
     disableForm();
     var geoOptions = { timeout: 8000 };
     var geoSuccess = function(position) {
       var lat = position.coords.latitude;
       var lng = position.coords.longitude;
       // success! onwards to view the content
-      submitLocation(lat, lng);
+      submitLocation(lat, lng, "");
     };
     var geoError = function(error) {
       console.log('Error finding your location: ' + error.message);
@@ -148,39 +255,6 @@ $( document ).ready(function() {
     };
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
   });
-
-  // during api calls, disable the form
-  function disableForm() {
-    $("#location-text").prop("disabled", true);
-    $("#location-submit").addClass("disabled");
-    $(".auto-location-submit").addClass("disabled");
-    $(".loading").show();
-  }
-
-  // if a search fails or a restart, enable the form
-  function enableForm() {
-    $("#location-text").prop("disabled", false);
-    $("#location-submit").removeClass("disabled");
-    $(".auto-location-submit").removeClass("disabled");
-    $(".loading").hide();
-  }
-
-  function submitLocation(lat,lng) {
-    // reload the page with the lat,lng
-    document.location =  encodeURI(document.location.pathname + "?lat=" + lat + "&lng=" + lng + "&loc=" + location_query_text);
-  }
-
-    // Set up slick photo slideshow
-  function loadGallery() {
-    var currentSlideElement = $('.disaster-content.active .past-photos');
-    currentSlideElement.slick({
-      slidesToShow: 1,
-      variableWidth: true,
-      prevArrow: '<button type="button" class="slick-prev"><</button>',
-      nextArrow: '<button type="button" class="slick-next">></button>'
-    });
-    return currentSlideElement;
-  }
 
   // Initialize the slide gallery on the open disaster tab
   var slideContainer = loadGallery();
@@ -193,8 +267,6 @@ $( document ).ready(function() {
 
 
 // Signup and login functionality
-
-  // Signup forms
 
   $(".button--signup").click(function() {
     $("#user-button-container").hide();
@@ -245,72 +317,15 @@ $( document ).ready(function() {
     );
   });
 
-  function setValueOnFocus(el, value) {
-    el.focus(function() {
-      if(el.val() === "") {
-        el.val(value);
-      }
-    });
-  }
-
-  function requiredFocus(el) {
-    el.focus(function() {
-      el.removeAttr('placeholder');
-    });
-  }
-
-  function requiredBlur(el, text) {
-    el.blur(function() {
-      if(el.val() === "") {
-        el.attr('placeholder', text);
-      }
-    });
-  }
-
   requiredFocus($("#user-signup__username"));
   requiredFocus($("#user-signup__password"));
   requiredBlur($("#user-signup__username"), "Valid email address required.");
   requiredBlur($("#user-signup__password"), "Required");
   setValueOnFocus($("#user-signup__state"), "WA");
 
-  var sendAjaxAuthRequest = function(url, data, error, success) {
-    var getCookie = function(name) {
-      var cookieValue = null;
-      if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-          var cookie = $.trim(cookies[i]);
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) == (name + '=')) {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-            }
-          }
-      }
-      return cookieValue;
-    }
-    var csrftoken = getCookie('csrftoken');
-    $.ajaxSetup({
-      crossDomain: false,
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      }
-    });
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: data,
-      error: error,
-      success: success
-    });
-  };
-
   $("#user-signup__submit").click(function() {
-    var inputs = $("#user-signup__form").find('input:visible');
-    for(var i = 0; i < inputs.length; i++ ) {
-      if(!inputs[i].checkValidity()) {
-        return false;
-      }
+    if(hasInvalidInput($("#user-signup__form"))) {
+      return false;
     }
 
     var username = $('#user-signup__username').val();
@@ -344,12 +359,10 @@ $( document ).ready(function() {
   });
 
   $("#user-login__submit").click(function() {
-    var inputs = $("#user-login__form").find('input:visible');
-    for(var i = 0; i < inputs.length; i++ ) {
-      if(!inputs[i].checkValidity()) {
-        return false;
-      }
+    if(hasInvalidInput($("#user-login__form"))) {
+      return false;
     }
+
     var username = $('#user-login__username').val();
     var password = $('#user-login__password').val();
 
@@ -373,12 +386,10 @@ $( document ).ready(function() {
   });
 
   $("#user-profile__submit").click(function() {
-    var inputs = $("#user-profile__form").find('input:visible');
-    for(var i = 0; i < inputs.length; i++ ) {
-      if(!inputs[i].checkValidity()) {
-        return false;
-      }
+    if(hasInvalidInput($("#user-profile__form"))) {
+      return false;
     }
+
     var address1 = $('#user-profile__address1').val();
     var address2 = $('#user-profile__address2').val();
     var city = $('#user-profile__city').val();
@@ -404,5 +415,4 @@ $( document ).ready(function() {
         $("#user-profile-result-container").show();
     });
   });
-
 });
