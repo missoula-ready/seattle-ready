df6d583a7b05b25e2364bd62f0a420c7f43d7319
nein09 <nein09@nein09.com>
2017-09-14 11:07:59 -0700

    This function got a max_length arg in newer Django versions

--- disasterinfosite/models.py
+++ disasterinfosite/models.py
@@ -873,7 +873,7 @@ class PastEventsPhoto(models.Model):
         return self.image.url
 
 class OverwriteStorage(FileSystemStorage):
-    def get_available_name(self, name):
+    def get_available_name(self, name, max_length=None):
         self.delete(name)
         return name
 
